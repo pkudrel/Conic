da1b9336fbc499160ea4dbdc97e4f57231dec554
Piotr Kudrel <piotr.kudrel@deneblab.com>
2016-07-11 15:35:12 +0200

    Fix file

--- bl.ps1
+++ bl.ps1
@@ -24,7 +24,6 @@ param(
 		$ib = (Join-Path $scriptsPath "\tools\ib\Invoke-Build.ps1")
     )
 
-
 $ValueNamesToExport =@("repoPath", "configPath", "scriptsPath",
  "toolsPath", "buildEnv", "buildTarget",  "buildPath","buildNumber", "gitCommitNumber",
  "buildDateTime" , "gitBranch", "buildMiscInfo")
