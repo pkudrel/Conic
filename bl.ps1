16ffcc2972808ede5adcaf004593b60eb27a5e2a
Piotr Kudrel <piotr.kudrel@deneblab.com>
2016-10-14 12:42:49 +0200

    Change build script

--- bl.ps1
+++ bl.ps1
@@ -13,15 +13,22 @@ param(
 		$buildPath = (Join-Path $repoPath "\build-out" ),
 		$toolsPath = (Join-Path $repoPath "\tools" ),
 		$configPath = (Join-Path $repoPath "\.config\build.json" ),
+		$config = (Get-Content $configPath | Out-String | ConvertFrom-Json),
+		$psGitVersionConfig = ([PSCustomObject]$config.PsGitVersion),
 		$buildScriptsPath = (Join-Path $scriptsPath  "default.ps1" ),
 		$buildTarget = "Release",
 		$buildEnv = "local",
-		$buildCounter = 0,
 		$buildMiscInfo = "",
+		$buildCounter = 0,
+		$buildMajor = $( if ($psGitVersionConfig.Major -ne $null) { $psGitVersionConfig.Major } else {0} ),
+		$buildMinor = $( if ($psGitVersionConfig.Minor -ne $null) { $psGitVersionConfig.Minor } else {0} ),
+		$buildPatch =  $( if ($psGitVersionConfig.Patch -ne $null) { $psGitVersionConfig.Patch } else {0} ),
+		$buildSpecial = $( if ($psGitVersionConfig.Special -ne $null) { $psGitVersionConfig.Special } else {""} ),
 		$buildDateTime = ((Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")),
 		$gitCommitNumber = 0,
 		$gitBranch = "",
-		$gitVersionStrategy = "standardOrCombinateMajorAndBuildSlice3",
+		
+		$psGitVersionStrategy = "standard",
 		$ib = (Join-Path $scriptsPath "\tools\ib\Invoke-Build.ps1")
     )
 
@@ -34,10 +41,10 @@ $ValueNamesToExport =@("repoPath", "configPath", "scriptsPath",
 . (Join-Path $scriptsPath "ps\misc.ps1")
 . (Join-Path $scriptsPath "ps\psgitversion.ps1")
 
+
 # make 
-$config = Get-Content $configPath | Out-String | ConvertFrom-Json
-$v = ([PSCustomObject]$config.SemVer)
-$bv = Get-GitVersion $gitVersionStrategy $v.major $v.minor $v.patch $buildCounter $v.special $buildEnv $gitBranch
+
+$bv = Get-GitVersion $psGitVersionStrategy $buildMajor $buildMinor $buildPatch $buildCounter $v.Special $buildEnv $gitBranch
 $global:psgitversion = $bv 
 
 
